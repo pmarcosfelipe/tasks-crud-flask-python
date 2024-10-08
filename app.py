3d93fa6bbc480caefe73db06958cba54c575c50c
Marcos Felipe <p.marcosfelipe@gmail.com>
2024-10-08 19:08:51 +0100

    created function to create task

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-  return "Hello World!"
-
-
-@app.route("/about")
-def about():
-  return "Page About!"
+tasks = []
+task_id_control = 1
+@app.route("/tasks", methods=["POST"])
+def create():
+  global task_id_control
+  data = request.get_json()
+  new_test = Task(id=task_id_control, title=data.get("title"), description=data.get("description", ""))
+  task_id_control += 1
+  tasks.append(new_test)
+  print(tasks)
+  return jsonify({ "message" : "New task created successfully!"})
 
 # validation for development server
 if __name__ == "__main__":
