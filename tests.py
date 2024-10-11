66497dc0f5ed9a5de3c030a5c09cdbe557a434ee
Marcos Felipe <p.marcosfelipe@gmail.com>
2024-10-11 20:59:35 +0100

    create test case for get_tasks and get_task_by_id

--- tests.py
+++ tests.py
@@ -18,3 +18,20 @@ def test_create_task():
   assert "id" in response_json
 
   TASKS.append(response_json["id"])
+
+
+def test_get_tasks():
+  response = requests.get(f"{BASE_URL}/tasks")
+  assert response.status_code == 200
+  response_json = response.json()
+  assert "tasks" in response_json
+  assert "total_tasks" in response_json
+
+def test_get_task_by_id():
+  if(TASKS):
+    task_id = TASKS[0]
+
+  response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+  assert response.status_code == 200
+  response_json = response.json()
+  assert task_id == response_json["id"]
