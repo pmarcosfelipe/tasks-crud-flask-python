a08457411e6295c56f3d57cb94c2fb95a50916d9
Marcos Felipe <p.marcosfelipe@gmail.com>
2024-10-12 12:19:03 +0100

    created tests case for update_task_by_id and delete_task_by_id

--- tests.py
+++ tests.py
@@ -27,6 +27,7 @@ def test_get_tasks():
   assert "tasks" in response_json
   assert "total_tasks" in response_json
 
+
 def test_get_task_by_id():
   if(TASKS):
     task_id = TASKS[0]
@@ -35,3 +36,36 @@ def test_get_task_by_id():
   assert response.status_code == 200
   response_json = response.json()
   assert task_id == response_json["id"]
+
+
+def test_update_task_by_id():
+  if(TASKS):
+    task_id = TASKS[0]
+
+    payload = {
+      "title":  "UPDATED Tast Title",
+      "description": "This is a Task description",
+      "completed": True
+    }
+
+    response = requests.put(f"{BASE_URL}/tasks/{task_id}", json=payload)
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "message" in response_json
+
+    response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert response_json["title"] == payload["title"]
+    assert response_json["description"] == payload["description"]
+    assert response_json["completed"] == payload["completed"]
+
+
+def test_delete_task_by_id():
+  if(TASKS):
+    task_id = TASKS[0]
+    response = requests.delete(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 200
+
+    response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 404
